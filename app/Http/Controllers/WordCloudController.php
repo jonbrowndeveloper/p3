6dacd6a3a951cb43bde13180d6cd73d32a377ee2
Jon Brown <jonbrowndeveloper@gmail.com>
2018-03-07 17:09:54 -0600

    added WordCloudController and routing

---
+++ app/Http/Controllers/WordCloudController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WordCloudController extends Controller
+{
+    public function home()
+    {
+        return 'not much going on right now...';
+    }
+
+}
